64d1f708374c02356a02dafb212b931dd8ec2884
Zahu2018 <35234001+Zahu2018@users.noreply.github.com>
2020-10-23 19:41:42 +0300

    Update picture_image_download.py

--- picture_image_download.py
+++ picture_image_download.py
@@ -1,4 +1,4 @@
 import urllib.request
 ...
 # Download the file from `url` and save it locally under `file_name`:
-urllib.request.urlretrieve("http://atkgalleria.ero.today/lingerie/342632/m07.jpg", "unu.jpg")
+urllib.request.urlretrieve("http://site/.../342632/foto.jpg", "unu.jpg")
